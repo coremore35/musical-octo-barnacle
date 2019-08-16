9a9d455fc3fa223b3c2d9c7cbb1b9ab436639f34
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-16 16:24:11 -0400

    added add/delete buttons

--- bookmarks_frontend/src/components/ShowBookmark.js
+++ bookmarks_frontend/src/components/ShowBookmark.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class ShowBookmark extends Component {
   render() {
     return (
-      <ul className="bookmark-ul">
+      <ul className='bookmark-ul'>
         {this.props.bookmarks.map(bookmark => {
           return (
-            <a className="bookmark-a" href={bookmark.url} key={bookmark._id}>
-              <li className="bookmark-li">{bookmark.title} </li>
-            </a>
+            <div>
+              <a className='bookmark-a' href={bookmark.url} key={bookmark._id}>
+                <li className='bookmark-li'>
+                  {bookmark.title} <button href='#'>EDIT</button>
+                  <button href='#'>DELETE</button>
+                </li>
+              </a>
+            </div>
           );
         })}
       </ul>
