b32a59fc8be7741c2b910e3b008c93909271de21
Mushnik <cmorrison@Coreys-Mac.local>
2019-08-16 08:41:00 -0400

    added component folder, begin on app.js

--- bookmarks_frontend/src/App.js
+++ bookmarks_frontend/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let baseURL = 'http://localhost:3003'
 class App extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        title: "",
+        
+      }
+  }
   render() {
     return (
       <div className='App'>
-        <h1>HOWDY!</h1>
+        <h1>Bookmark'd!</h1>
+        <
       </div>
     );
   }
